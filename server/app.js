dd443d13b27fb8e4bb5b3a8ffee77b866d76c994
argygeor <signup@arggeo.com>
2021-03-02 23:48:49 +0200

    Replaced REST API with GRAPHQL

--- server/app.js
+++ server/app.js
@@ -5,9 +5,12 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
+const graphqlHttp = require('express-graphql').graphqlHTTP;
 
-const feedRoutes = require('./routes/feed');
-const authRoutes = require('./routes/auth');
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
+const auth = require('./middleware/auth');
+const { clearImage } = require('./util/file');
 
 const app = express();
 
@@ -35,13 +38,50 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
    res.setHeader('Access-Control-Allow-Origin', '*');
-   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+   if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+   }
    next();
 });
 
-app.use('/feed', feedRoutes);
-app.use('/auth', authRoutes);
+app.use(auth);
+
+app.put('/post-image', (req, res, next) => {
+   if (!req.isAuth) {
+      throw new Error('Not authenticated!');
+   }
+   if (!req.file) {
+      return res.status(200).json({ message: 'No file provided!' });
+   }
+   if (req.body.oldPath) {
+      clearImage(req.body.oldPath);
+   }
+   return res.status(201).json({
+      message: 'File stored.',
+      filePath: req.file.path
+   });
+});
+
+app.use('/graphql', graphqlHttp({
+   schema: graphqlSchema,
+   rootValue: graphqlResolver,
+   graphiql: true,
+   formatError(err) {
+      if (!err.originalError) {
+         return err;
+      }
+      const data = err.originalError.data;
+      const message = err.message || 'An error occurred.';
+      const code = err.originalError.code || 500;
+      return {
+         message: message,
+         status: code,
+         data: data
+      };
+   }
+}));
 
 app.use((error, req, res, next) => {
    console.log(error);
@@ -56,10 +96,6 @@ app.use((error, req, res, next) => {
 
 mongoose.connect('mongodb+srv://argy:myS3cuReP4ssW0rd@cluster0.elrli.mongodb.net/messages')
    .then(result => {
-      const server = app.listen(8080);
-      const io = require('./socket').init(server);
-      io.on('connection', socket => {
-         console.log('Client connected');
-      });
+      app.listen(8080);
    })
    .catch(err => console.log(err));
